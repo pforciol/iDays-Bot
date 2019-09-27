3626ba6531931f18778d323456ad7b27cb7e1cfc
tpompon <thpompon@gmail.com>
2019-09-27 23:32:06 +0200

    Bot almost done, should be working fine but days.json not finished

--- bot.js
+++ bot.js
@@ -1,11 +1,160 @@
-// bot.js
 
+const express = require('express');
 const Twitter = require('twitter');
 const dotenv = require('dotenv').config();
 const config = require('./config.js');
+const daysJSON = require('./days.json');
+
+const app = express();
 const client = new Twitter(config);
 
-client.post('statuses/update', {status: 'I Love Twitter'}, (error, tweet, response) => {
-	console.log(tweet);
-});
+// app.get('/', (req, res) => {
+//   res.json({ status: "Welcome on iDays API"});
+// })
 
+const months = new Array();
+months[0] = "janvier";
+months[1] = "fevrier";
+months[2] = "mars";
+months[3] = "avril";
+months[4] = "mai";
+months[5] = "juin";
+months[6] = "juillet";
+months[7] = "aout";
+months[8] = "septembre";
+months[9] = "octobre";
+months[10] = "novembre";
+months[11] = "decembre";
+
+getDays = (day) => {
+	const d = new Date();
+	const month = d.getMonth();
+	const days = [];
+
+	if (day === 'lundi') {
+		day = 1;
+	} else if (day === 'mardi') {
+		day = 2;
+	} else if (day === 'mercredi') {
+		day = 3;
+	} else if (day === 'jeudi') {
+		day = 4;
+	} else if (day === 'vendredi') {
+		day = 5;
+	} else if (day === 'samedi') {
+		day = 6;
+	} else if (day === 'dimanche') {
+		day = 0;
+	}
+
+	d.setDate(1);
+	while (d.getDay() !== day) {
+		d.setDate(d.getDate() + 1);
+	}
+	while (d.getMonth() === month) {
+		days.push(new Date(d.getTime()).getDate());
+		d.setDate(d.getDate() + 7);
+	}
+	return days;
+}
+
+const d = new Date();
+const now = {
+	day: d.getDate(), // Day
+	weekday: d.getDay(), // 0 to 6 - Sunday to Saturday
+	month: months[d.getMonth()],
+	year: d.getFullYear() // Year
+}
+
+const days = daysJSON[now.month].find(obj => obj.day == now.day);
+const specificDays = daysJSON[now.month].filter(obj => obj.day.startsWith('specific'));
+
+const publishTweets = (days, specificDays) => {
+
+	const d = new Date();
+	const publishedTweets = [];
+
+	if (days) {
+		if (days.entries) {
+			days.entries.map(day => {
+				const tweet = `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}\n\n${day.emoji} ${day.content}\n${day.link}`;
+				
+				client.post('statuses/update', {status: tweet}, (error, tweetData, response) => {
+					if (error) console.log(error);
+				});
+	
+				publishedTweets.push(tweet);
+			})
+		} else {
+			const tweet = `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}\n\n${days.emoji} ${days.content}\n${days.link}`;
+			
+			client.post('statuses/update', {status: tweet}, (error, tweetData, response) => {
+				if (error) console.log(error);
+			});
+	
+			publishedTweets.push(tweet);
+		}
+	}
+
+	if (specificDays) {
+
+		specificDays.map(specificDay => {
+			const sp = specificDay.day.split('_');
+			const daysOfMonth = getDays(sp[2]);
+
+			if (sp[1] === '1' && daysOfMonth[0] && daysOfMonth[0] === now.day) {
+				const tweet = `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}\n\n${specificDay.emoji} ${specificDay.content}\n${specificDay.link}`;
+				
+				client.post('statuses/update', {status: tweet}, (error, tweetData, response) => {
+					if (error) console.log(error);
+				});
+
+				publishedTweets.push(tweet);
+			} else if (sp[1] === '2' && daysOfMonth[1] && daysOfMonth[1] === now.day) {
+				const tweet = `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}\n\n${specificDay.emoji} ${specificDay.content}\n${specificDay.link}`;
+				
+				client.post('statuses/update', {status: tweet}, (error, tweetData, response) => {
+					if (error) console.log(error);
+				});
+
+				publishedTweets.push(tweet);
+			} else if (sp[1] === '3' && daysOfMonth[2] && daysOfMonth[2] === now.day) {
+				const tweet = `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}\n\n${specificDay.emoji} ${specificDay.content}\n${specificDay.link}`;
+
+				client.post('statuses/update', {status: tweet}, (error, tweetData, response) => {
+					if (error) console.log(error);
+				});
+
+				publishedTweets.push(tweet);
+			} else if (sp[1] === '4' && daysOfMonth[3] && daysOfMonth[3] === now.day) {
+				const tweet = `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}\n\n${specificDay.emoji} ${specificDay.content}\n${specificDay.link}`;
+				
+				client.post('statuses/update', {status: tweet}, (error, tweetData, response) => {
+					if (error) console.log(error);
+				});
+
+				publishedTweets.push(tweet);
+			} else if (sp[1] === 'last' && daysOfMonth[daysOfMonth.length - 1] && daysOfMonth[daysOfMonth.length - 1] === now.day) {
+				const tweet = `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}\n\n${specificDay.emoji} ${specificDay.content}\n${specificDay.link}`;
+
+				client.post('statuses/update', {status: tweet}, (error, tweetData, response) => {
+					if (error) console.log(error);
+				});
+
+				publishedTweets.push(tweet);
+			}
+		})
+	}
+
+	return publishedTweets;
+}
+
+console.log(publishTweets(days, specificDays));
+
+// client.post('statuses/update', {status: `${daysJSON.janvier[3].emoji} - ${daysJSON.janvier[3].content}`}, (error, tweet, response) => {
+// 	console.log(tweet);
+// });
+
+// app.listen(config.PORT, () => {
+//   console.log(`Server is ready on: http://localhost:${config.PORT}/`);
+// })
--- config.js
+++ config.js
@@ -1,8 +1,7 @@
-// config.js
-
 module.exports = {
 	consumer_key: process.env.TWITTER_CONSUMER_KEY,
 	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
 	access_token_key: process.env.TWITTER_ACCES_TOKEN_KEY,
-	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+	PORT: 3000
 }
